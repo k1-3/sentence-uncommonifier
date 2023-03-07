3f4e82302d4a91d4c12bbbf391a641256dfd7045
k1-3 <126980148+k1-3@users.noreply.github.com>
2023-03-06 17:01:46 -0800

    Update sentence-uncommonifier.py

--- sentence-uncommonifier.py
+++ sentence-uncommonifier.py
@@ -1,6 +1,6 @@
 '''
 Sentence Uncommonifier
-By Justin Sun(@host008 on github)
+By Justin Sun(@host008 on github)(This is an alt)
 
 takes a sentence and replaces all the common words with uncommon words
 
@@ -15,15 +15,13 @@ from nltk.corpus import wordnet
 import nltk
 from wordfreq import word_frequency
 import logging
-
+import random
 # nltk setup
 nltk.download('wordnet')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('omw-1.4')
 
-# words to ignore
-ignore_words = 'a an the and but or for of at i'.split(' ')
 # functions
 '''
 def find_uncommon_synonym(word: str) -> str:
@@ -52,7 +50,7 @@ def convert_pos(pos: str) -> str:
         return wordnet.VERB
     else:
         return ''
-def find_uncommon_synonym(word: str, context: str) -> str:
+def find_uncommon_synonym(word: str, context: str, randomness: int = 0) -> str:
     
     synonyms: list = []
     partofspeech: str = ''
@@ -68,10 +66,6 @@ def find_uncommon_synonym(word: str, context: str) -> str:
         for l in syn.lemmas():
             synonyms.append(l.name())
         
-    
-    if word in ignore_words: # if the word is in the ignore list, return the word
-        return word
-    
     else:
         if len(synonyms) < 1: # if the word has no synonyms, return the word
             return word
@@ -80,12 +74,23 @@ def find_uncommon_synonym(word: str, context: str) -> str:
             return synonyms[0].replace('_', ' ')
         
         else:
-            least_common_word: str = word
+            sorted_list_by_rarity: list = [word]
             for synonym in synonyms:
-                if word_frequency(synonym, 'en') < word_frequency(least_common_word, 'en'):
-                    least_common_word = synonym
-            return least_common_word.replace('_', ' ')
-
+                if word_frequency(synonym, 'en') < word_frequency(sorted_list_by_rarity[0], 'en'): # if the synonym is less common than the first word in the list, insert it at the beginning
+                    sorted_list_by_rarity.insert(0, synonym)
+                elif word_frequency(synonym, 'en') > word_frequency(sorted_list_by_rarity[-1], 'en'): # if the synonym is more common than the last word in the list, append it to the end
+                    sorted_list_by_rarity.append(synonym)
+                else:
+                    for i in range(len(sorted_list_by_rarity)):
+                        if word_frequency(synonym, 'en') > word_frequency(sorted_list_by_rarity[i], 'en'): # if the synonym is more common than the word at the current index, insert it at the current index
+                            sorted_list_by_rarity.insert(i, synonym)
+                            break
+            if randomness == 0:
+                return sorted_list_by_rarity[0].replace('_', ' ')
+            if randomness > len(sorted_list_by_rarity):
+                return sorted_list_by_rarity[random.randint(0, len(sorted_list_by_rarity) - 1)].replace('_', ' ') # Edge case: if the randomness is greater than the length of the list, return a random word from the list
+            else:
+                return sorted_list_by_rarity[random.randint(0, len())].replace('_', ' ')
 def uncommonify_sentence(sentence: str) -> str:
     words: list = sentence.split(' ')
     uncommonified_words: list = []
@@ -97,3 +102,5 @@ def uncommonify_sentence(sentence: str) -> str:
 if __name__ == '__main__':
     sentence: str = input('Enter a sentence: ')
     print(uncommonify_sentence(sentence))
+    
+    
